3540f1430f0634a2ef49b828da8a06fef0608126
TruongTX <me@truongtx.me>
2018-05-07 20:53:51 +0700

    Add class Deque

---
+++ src/Deque.java
@@ -0,0 +1,40 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+    public Deque() {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void addFirst(Item item) {
+
+    }
+
+    public void addLast(Item item) {
+
+    }
+
+    public Item removeFirst() {
+        return null;
+    }
+
+    public Item removeLast() {
+        return null;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
