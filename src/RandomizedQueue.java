9ee1742c2e7f36ffa99dc9320df29947f22eb50c
TruongTX <me@truongtx.me>
2018-05-08 21:58:42 +0700

    Implement resize method

--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -1,19 +1,36 @@
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.Iterator;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
+    private Item[] q; // store queue data
+    private int head;
+    private int tail;
+    private int capacity;
+
     public RandomizedQueue() {
+        // init the queue as size 1
+        this.capacity = 1;
+        this.q = (Item[]) new Object[capacity];
+
+        this.head = 0;
+        this.tail = 0;
     }
 
     public boolean isEmpty() {
-        return false;
+        return this.size() == 0;
     }
 
     public int size() {
-        return 0;
+        return this.tail - this.head;
     }
 
     public void enqueue(Item item) {
+        int size = this.size();
 
+        if (size == this.capacity) {
+            this.resize(this.capacity * 2);
+        }
     }
 
     public Item dequeue() {
@@ -28,6 +45,29 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         return null;
     }
 
+    private void resize(int newCap) {
+        // init new queue
+        Item[] newQ = (Item[]) new Object[newCap];
+
+        // copy to new queue
+        int newIndex;
+        int i;
+        for (i = head, newIndex = 0; i < tail; i++, newIndex++) {
+            int oldIndex = this.getIndex(i);
+            newQ[newIndex] = this.q[oldIndex];
+        }
+
+        this.q = newQ;
+        this.capacity = newCap;
+        this.head = 0;
+        this.tail = newIndex;
+    }
+
+    private int getIndex(int index) {
+        return index % this.capacity;
+    }
+
     public static void main(String[] args) {
+
     }
 }
