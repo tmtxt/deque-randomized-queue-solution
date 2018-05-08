1f38c85e26bbffcf5b6ba6d42c1301753694a1fe
TruongTX <me@truongtx.me>
2018-05-08 21:18:24 +0700

    Add method structure

--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -1,2 +1,33 @@
-public class RandomizedQueue {
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    public RandomizedQueue() {
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void enqueue(Item item) {
+
+    }
+
+    public Item dequeue() {
+        return null;
+    }
+
+    public Item sample() {
+        return null;
+    }
+
+    public Iterator<Item> iterator() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+    }
 }
