18e59c9a3210a3804115835c039698af0f9bc456
TruongTX <me@truongtx.me>
2018-05-09 20:18:30 +0700

    Implement dequeue function

--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -2,6 +2,7 @@ import edu.princeton.cs.algs4.StdRandom;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
     private Item[] q; // store queue data
@@ -27,6 +28,10 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     public void enqueue(Item item) {
+        if (item == null) {
+            throw new IllegalArgumentException("Item cannot be null");
+        }
+
         int size = this.size();
 
         if (size == this.capacity) {
@@ -39,7 +44,28 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     public Item dequeue() {
-        return null;
+        int size = this.size();
+        if (size == 0) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+
+        // get item from random index
+        int randomIndex = StdRandom.uniform(this.head, this.tail);
+        int index = this.getIndex(randomIndex);
+        Item item = this.q[index];
+
+        // move the last item to the removed item index
+        int lastIndex = this.getIndex(this.tail - 1);
+        if (randomIndex != this.tail - 1) { // only perform
+            Item lastItem = this.q[lastIndex];
+            this.q[index] = lastItem;
+        }
+
+        // remove the last item
+        this.q[lastIndex] = null;
+        this.tail--;
+
+        return item;
     }
 
     public Item sample() {
@@ -95,6 +121,13 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         System.out.println("enqueue def");
         queue.enqueue("def");
         System.out.println(queue.size());
+        System.out.println(Arrays.toString(queue.q));
+        System.out.println(queue.head);
+        System.out.println(queue.tail);
+
+        System.out.println("---");
+        System.out.println("dequeue");
+        System.out.println(queue.dequeue());
         System.out.println(Arrays.toString(queue.q));
         System.out.println(queue.head);
         System.out.println(queue.tail);
