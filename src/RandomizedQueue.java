f42c7fcf3db3e769b03f04eb27c27d2b321a1058
TruongTX <me@truongtx.me>
2018-05-09 20:28:54 +0700

    Implement sample method

--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -6,7 +6,6 @@ import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
     private Item[] q; // store queue data
-    private int head;
     private int tail;
     private int capacity;
 
@@ -15,7 +14,6 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         this.capacity = 1;
         this.q = (Item[]) new Object[capacity];
 
-        this.head = 0;
         this.tail = 0;
     }
 
@@ -24,7 +22,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     public int size() {
-        return this.tail - this.head;
+        return this.tail;
     }
 
     public void enqueue(Item item) {
@@ -49,7 +47,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         }
 
         // get item from random index
-        int randomIndex = StdRandom.uniform(this.head, this.tail);
+        int randomIndex = StdRandom.uniform(this.tail);
         Item item = this.q[randomIndex];
 
         // move the last item to the removed item index
@@ -63,11 +61,23 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         this.q[lastIndex] = null;
         this.tail--;
 
+        if (this.size() == this.capacity / 4) {
+            this.resize(this.capacity / 2);
+        }
+
         return item;
     }
 
     public Item sample() {
-        return null;
+        int size = this.size();
+        if (size == 0) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+
+        int randomIndex = StdRandom.uniform(this.tail);
+        Item item = this.q[randomIndex];
+
+        return item;
     }
 
     public Iterator<Item> iterator() {
@@ -80,13 +90,12 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
         // copy to new queue
         int i;
-        for (i = head; i < tail; i++) {
+        for (i = 0; i < tail; i++) {
             newQ[i] = this.q[i];
         }
 
         this.q = newQ;
         this.capacity = newCap;
-        this.head = 0;
         this.tail = i;
     }
 
@@ -100,28 +109,29 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         queue.enqueue("hello");
         System.out.println(queue.size());
         System.out.println(Arrays.toString(queue.q));
-        System.out.println(queue.head);
         System.out.println(queue.tail);
 
         System.out.println("enqueue abc");
         queue.enqueue("abc");
         System.out.println(queue.size());
         System.out.println(Arrays.toString(queue.q));
-        System.out.println(queue.head);
         System.out.println(queue.tail);
 
         System.out.println("enqueue def");
         queue.enqueue("def");
         System.out.println(queue.size());
         System.out.println(Arrays.toString(queue.q));
-        System.out.println(queue.head);
         System.out.println(queue.tail);
 
         System.out.println("---");
         System.out.println("dequeue");
         System.out.println(queue.dequeue());
         System.out.println(Arrays.toString(queue.q));
-        System.out.println(queue.head);
+        System.out.println(queue.tail);
+
+        System.out.println("dequeue");
+        System.out.println(queue.dequeue());
+        System.out.println(Arrays.toString(queue.q));
         System.out.println(queue.tail);
     }
 }
